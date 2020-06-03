7bb19c58ad03265ac8b03557adb1be695133b905
kimberlybone <k_bone@uncg.edu>
2020-06-03 19:32:44 -0400

    First program

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
-print('This is my first python file for the course')
 
-# Get the name of the file and open it
+# Convert elevator floors
 
+inp = input('European floor? ')
+usFloor = int(inp) + 1
 
+print('The US Floor number is:', usFloor)
